0a52b97778e08a9f909cd098673e8032321c3608
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-13 15:42:48 +0600

    new component added to homepage

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
+import Professional from '../Professional/Professional';
 
 const Home = () => {
     return (
         <div>
             <Carousel />
-            Home
+            <Professional />
         </div>
     );
 };
---
+++ src/Pages/HomePage/Professional/Professional.jsx
@@ -0,0 +1,58 @@
+import { NavLink } from 'react-router-dom';
+import professional from '../../../assets/Professional/professional(1).jpg';
+import { FaArrowRight } from 'react-icons/fa';
+import { GiAutoRepair } from 'react-icons/gi';
+import { MdOutlineTireRepair } from 'react-icons/md';
+import { MdCarRepair } from 'react-icons/md';
+import './Professional.css';
+import Title from '../../Shared/Title/Title';
+
+const Professional = () => {
+  return (
+    <section className="flex flex-col lg:flex-row my-10 lg:my-32 mx-auto max-w-screen-xl">
+      {/* Left div */}
+      <div className="lg:flex-1 relative order-2 lg:order-1 mt-5">
+        <img src={professional} alt="" />
+        <div id="horizontally-align-text" className="flex justify-evenly absolute w-5/6 -mt-14 ml-10 p-2 bg-mainColor parallelogram">
+          <div className="parallelogram1">
+            <MdCarRepair className="text-4xl lg:text-6xl text-white" />{' '}
+            <h4 className="text-white text-xs lg:text-sm font-bold">
+              Certified <br />Repair
+            </h4>
+          </div>
+          <div className="parallelogram1">
+            <GiAutoRepair className="text-4xl lg:text-6xl text-white" />{' '}
+            <h4 className="text-white text-xs lg:text-sm font-bold">
+              Certified <br />  Service
+            </h4>
+          </div>
+          <div className="parallelogram1">
+            <MdOutlineTireRepair className="text-4xl lg:text-6xl text-white" />{' '}
+            <h4 className="text-white text-xs lg:text-sm font-bold">
+              Auto <br /> Repair
+            </h4>
+          </div>
+        </div>
+      </div>
+      {/* Right div */}
+      <div className="lg:flex-1 pl-5 order-1 lg:order-2 mt-5">
+      {/* <div className="lg:flex-1 pl-5 order-1 lg:order-2 bg-gradient-to-r from-[#FF4B2B] to-[#FF416C] text-white"> */}
+        <Title text="CAR SERVICES WITH YOU IN MIND"></Title>
+        <h2 className="text-2xl lg:text-6xl font-bold">
+          WE ARE A PROFESSIONAL AUTO REPAIR SERVICE
+        </h2>
+        <p className="my-8 text-justify">
+          AutoTech is a well-established auto repair shop in Sydney, Brisbane, Parth.
+          Our company offers car maintenance to major repairs since 1971. We are
+          a company that has built its reputation on reliable services for all
+          times of vehicles in Sydney, Australia.
+        </p>
+        <NavLink className="flex items-center">
+          MORE ABOUT US <FaArrowRight className="inline ml-2" />{' '}
+        </NavLink>
+      </div>
+    </section>
+  );
+};
+
+export default Professional;
