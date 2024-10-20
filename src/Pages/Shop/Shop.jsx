73bfbbbe7435b65d6bfe88044d70ba69195e59cf
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-20 14:54:08 +0600

    shops tab section design done

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import shopCover from '../../assets/shop/shop-cover.webp'
+import soundTab from '../../assets/shop/soundTab.avif'
+import wheelsTab from '../../assets/shop/wheelsTab.avif'
+import featuredTab from '../../assets/shop/featuredTab.avif'
 
 const Shop = () => {
     return (
         <section>
+            {/* Shop Cover */}
             <div className="hero h-96"
                 style={{
                     backgroundImage: `url(${shopCover})`,
@@ -14,6 +18,23 @@ const Shop = () => {
                         <h1 className="mb-5 text-5xl font-bold font-mono">Products</h1>
                         <p className="mb-5">Home / Products</p>
                     </div>
+                </div>
+            </div>
+
+
+            {/* Shop Tab */}
+            <div className='flex gap-5 justify-center py-10 bg-white'>
+                <div>
+                    <img src={soundTab} alt="" className='border-4 h-52 rounded-full' />
+                    <p className='hover:text-mainColor text-center pt-4 text-xl font-semibold font-mono'>Sounds</p>
+                </div>
+                <div>
+                    <img src={wheelsTab} alt="" className='border-4 h-52 rounded-full' />
+                    <p className='hover:text-mainColor text-center pt-4 text-xl font-semibold font-mono'>Wheels</p>
+                </div>
+                <div>
+                    <img src={featuredTab} alt="" className='border-4 h-52 rounded-full' />
+                    <p className='hover:text-mainColor text-center pt-4 text-xl font-semibold font-mono'>Featured</p>
                 </div>
             </div>
         </section>
