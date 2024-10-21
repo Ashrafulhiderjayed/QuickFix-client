2c5b216905a32fe212d3d27f558e6d644515fc36
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-21 17:15:52 +0600

    redesign cart

--- src/Pages/Shop/ShopCard.jsx
+++ src/Pages/Shop/ShopCard.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoCartSharp } from "react-icons/io5";
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
 
 const ShopCard = ({ product }) => {
-    const { name, image, price, rating, category } = product;
+    const { name, image, price, category } = product;
+    const [rating, setRating] = useState(4) // Initial value
     return (
         <div className="card bg-base-100 border-2">
             <figure>
                 <img src={image} alt="Shoes" className='h-50 w-full' />
             </figure>
             <div className="card-body">
+            <div className="text-right"><p className='badge badge-secondary'>NEW</p></div>
+                <Rating style={{ maxWidth: 120, height: '15px' }} value={rating} onChange={setRating} />
                 <h2 className="card-title">
                     {name}
-                    <div className="badge badge-secondary">NEW</div>
+                    {/* <div className="badge badge-secondary">NEW</div> */}
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p className='text-red-500 font-semibold text-xl'>${price}</p>
                 <div className="card-actions justify-center">
                     <button className='btn px-20 rounded-full hover:bg-mainColor hover:text-white'> <IoCartSharp /> Add to Cart</button>
                 </div>
