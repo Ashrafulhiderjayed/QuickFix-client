9eb2d8439eeb42a00e0db909e6c2b401e2176ab3
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-26 16:24:12 +0600

    redesign captcha

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,9 @@
 import { FaFacebookF } from "react-icons/fa";
 import { IoLogoGoogleplus } from "react-icons/io";
 import { FaLinkedinIn } from "react-icons/fa";
+import { FiRefreshCw } from "react-icons/fi";
 import './Login.css'
-// import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { loadCaptchaEnginge, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useContext, useEffect, useRef, useState } from "react";
 import Swal from "sweetalert2";
@@ -19,9 +20,12 @@ const Login = () => {
 
     const from = location.state?.From?.pathname || "/";
 
-    // useEffect(() => {
-    //     loadCaptchaEnginge(6);
-    // }, [])
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+      }, [])
+      const handleReloadCaptcha = () => {
+        loadCaptchaEnginge(6); // reloads the captcha
+    };
 
     const handleLogin = event => {
         event.preventDefault();
@@ -47,17 +51,17 @@ const Login = () => {
     }
 
 
-    // const handleValidateCaptcha = () => {
-    //     const user_captcha_value = captchaRef.current.value;
-    //     if (validateCaptcha(user_captcha_value)) {
-    //         setDisabled(false);
-    //         console.log('Captcha validated successfully');
-    //     }
-    //     else {
-    //         setDisabled(true);
-    //         console.log('Captcha validation failed');
-    //     }
-    // }
+    const handleValidateCaptcha = () => {
+        const user_captcha_value = captchaRef.current.value;
+        if (validateCaptcha(user_captcha_value)) {
+            setDisabled(false);
+            console.log('Captcha validated successfully');
+        }
+        else {
+            setDisabled(true);
+            console.log('Captcha validation failed');
+        }
+    }
 
 
     // const handleGoogleSignIn = () => {
@@ -97,6 +101,14 @@ const Login = () => {
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="password" name="password" placeholder="password" className="input bg-slate-100" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <LoadCanvasTemplateNoReload />
+                                <FiRefreshCw onClick={handleReloadCaptcha} className="cursor-pointer ml-2 text-lg" />
+                            </label>
+                            <input type="text" ref={captchaRef} name="captcha" placeholder="Enter Captcha" className="input bg-slate-100" required />
+                            <button onClick={handleValidateCaptcha} className="btn btn-xs btn-outline mt-2">Validate</button>
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
