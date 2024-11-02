d5ddcdb96202061d039c7317022c66af48457434
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-11-02 14:44:44 +0600

    refetch cart data using tanstackQuery

--- src/Pages/Shop/ShopCard.jsx
+++ src/Pages/Shop/ShopCard.jsx
@@ -6,14 +6,16 @@ import useAuth from '../../hooks/useAuth';
 import Swal from 'sweetalert2';
 import axios from 'axios';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
+import useCart from '../../hooks/useCart';
 
 const ShopCard = ({ product }) => {
     const { name, image, price, category, _id } = product;
     const {user} = useAuth();
     const [rating, setRating] = useState(4) // Initial value
     const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCart();
 
-    const handleAddToCart = (product) => {
+    const handleAddToCart = () => {
         if(user) {
             // Add product to user's cart
             const cartItem = {
@@ -30,6 +32,7 @@ const ShopCard = ({ product }) => {
                     icon: "success",
                     title: `${name} has been added to your cart!`,
                   });
+                  refetch(); // Fetch updated cart items after adding a product to the cart
             })
              .catch(err => {
                  console.error(err);
@@ -64,7 +67,7 @@ const ShopCard = ({ product }) => {
                 </h2>
                 <p className='text-red-500 font-semibold text-xl'>${price}</p>
                 <div className="card-actions justify-center">
-                    <button onClick={() => handleAddToCart(product)} className='btn px-20 rounded-full hover:bg-mainColor hover:text-white'> <IoCartSharp /> Add to Cart</button>
+                    <button onClick={handleAddToCart} className='btn px-20 rounded-full hover:bg-mainColor hover:text-white'> <IoCartSharp /> Add to Cart</button>
                 </div>
             </div>
         </div>
