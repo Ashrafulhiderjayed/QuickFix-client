8b3f94a2fb822c746d82554a325f8372f0aeb3a1
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-11-10 16:58:43 +0600

    payment page

--- src/Pages/Dashboard/UserDashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/UserDashboard/Payment/Payment.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
     return (
         <div>
-            Payment
+            <p>Payment</p>
+            <div>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
         </div>
     );
 };
